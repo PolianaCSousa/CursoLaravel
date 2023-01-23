046446e7aca1cf677b152d35410ae52538cf8da2
Poliana <0056154@academico.ifmg.edu.br>
2023-01-23 17:34:12 -0300

    Upload de imagem

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -30,6 +30,19 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        //image upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) { //verifica se a imagem inserida é válida
+
+            $requestImage = $request->image; //a variavel requestImage vai receber a imagem
+            $extension = $requestImage->extension(); //o método extension retorna a extensão da imagem
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now") . "." . $extension); //concatenando o nome da imagem com a data que ele foi salva 
+            //A função md5() converte a string em um hash md5 (criptografia)
+            $requestImage->move(public_path('img/events'), $imageName); //salvando a imagem na pasta /img/events
+
+            $event->image = $imageName; //o que será salvo de fato no BD será o  nome da imagem apenas
+        }
+
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
