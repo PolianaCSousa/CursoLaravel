789642c0751c13be5e83a82bea44a069b8aa2673
Poliana <0056154@academico.ifmg.edu.br>
2023-01-31 10:26:49 -0300

    Autenticação no Laravel com Jetstream e Livewire

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -27,21 +27,28 @@
                         <li class="nav-item">
                             <a href="/" class="nav-link">Eventos</a>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav">
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Criar Eventos</a>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav">
+                        @auth
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
+                            <a href="/dashboard" class="nav-link">Meus Eventos</a>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <a href="/logout" class="nav-link" onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+                            </form>
+                        </li>  
+                        @endauth 
+                        @guest      
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link">Entrar</a>
+                        </li>                  
+                        <li class="nav-item">
+                            <a href="/register" class="nav-link">Cadastrar</a>
                         </li>
+                        @endguest
                     </ul>
                 </div>
             </nav>
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -35,3 +35,13 @@ Route::get('/contato', function () {
     return view('contato');
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
