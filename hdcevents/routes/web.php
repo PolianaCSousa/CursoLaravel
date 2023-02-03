05f05c15f56f95d528f0208860d64ec5dfdbc0b0
Poliana <0056154@academico.ifmg.edu.br>
2023-02-03 09:12:04 -0300

    Relaçao many to many

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -19,15 +19,24 @@ Ponta final: eu retorno para o usuário o fim/resultado da manipulação dos dad
 Uma rota também pode retornar valores de variáveis por exemplo*/
 
 use App\Http\Controllers\EventController;
+use App\Models\Event;
 
 Route::get('/', [EventController::class, 'index']);
 //para criar um evento, é preciso estar logado. O middleware('aut') faz com que seja necessário estar logado para acessar a view de criação de eventos
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
+
 Route::get('/events/{id}', [EventController::class, 'show']);
+
 Route::get('/dashboard',[EventController::class, 'dashboard'])->middleware('auth');
+
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+
 Route::post('/events', [EventController::class, 'store']);
+
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
 Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+
 Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 
 //criei uma nova rota cujo URL é /contato e que retona a view contato (contato.blade.php). Lembrando que a url não precisa ser igual À view (não precisa ter o mesmo nome). Eu também posso retornar a mesma view para urls diferentes
